cf968cf1fa9f64336a60af4b2245e09b7acd8f97
baumpyru <dlee0@terpmail.umd.edu>
2024-05-15 19:33:09 -0400

    Update word_guessing_game.py
    Ask name from player and add intro text

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -2,17 +2,29 @@
 # Import modules for later use in program
 import random
 
+# Get player information (name)
+name = input("Enter your name:")
+
+# Tell player a quick intro to word guessing game
+# Test print by defining name
+name = "UniqueName"
+print(f"Hello {name}, this game's premise is simple, guess the animal name or a letter before you run out of tries.\nYou start with 25 tries, getting to the highest number wins.")
+print(''' ██████   ██████   ██████  ██████      ██      ██    ██  ██████ ██   ██ ██ 
+██       ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██  ██ 
+██   ███ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      █████   ██ 
+██    ██ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██     
+ ██████   ██████   ██████  ██████      ███████  ██████   ██████ ██   ██ ██''')
+
 # Randomly choose word from wordbank
 def randomanimal():
     with open('wordbank.txt','r') as file:
         words = file.readlines()
-        return random.choice(words)
-
+        return random.choice(words).strip()
 # Test random word
 # print(randomanimal())
 
 # Game code
-def guess_word():
+# def guess_word():
     
 
 
