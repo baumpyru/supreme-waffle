d7bc73b7bbf53e0192fe007646163557cb44dd41
baumpyru <dlee0@terpmail.umd.edu>
2024-05-15 21:44:19 -0400

    Add giveup function
    Add a giveup function where you can stop guessing and find out what the word was. Also listed new function in read me so the player know this exists

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ The word guessing game will be choose a random animal in `wordbank.txt` and ask 
 ## How to play
 - Enter your name to get started
 - Keep guessing letters or animals till you are correct
+- Type `giveup` if you no longer want to guess
 
 ## How to run
 **Make sure you have any version of python installed before runniing!**
 1. Clone repository or Download ZIP
 2. Path to downloaded repo folder in command window
-    *If you downloaded .ZIP unpack it*
+    *If you downloaded .ZIP unpack it first*
 3. Run `word_guessing_game.py`
 4. Have fun! :>
--- word_guessing_game.py
+++ word_guessing_game.py
@@ -6,9 +6,7 @@ import random
 name = input("Enter your name:")
 
 # Tell player a quick intro to word guessing game
-# Test print by defining name
-#name = "UniqueName"
-print(f"Hello {name}, this game's premise is simple, guess the animal name or a letter before you run out of tries.\nYou start with 25 tries, getting to the highest number wins.")
+print(f"Hello {name}, this game's premise is simple, guess the animal name or a letter before you run out of tries.\nYou start with 25 tries, getting to the highest number wins!")
 print(''' ██████   ██████   ██████  ██████      ██      ██    ██  ██████ ██   ██ ██ 
 ██       ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██  ██ 
 ██   ███ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      █████   ██ 
@@ -26,14 +24,14 @@ def randomanimal():
 # Game code
 def guess_word():
     word = randomanimal()
-    # Test Game code
-    # Comment/Remove later so the animal isn't always hippo
-    word = "hippo"
     tries = 25
     correct_guess = False
     while not correct_guess and tries > 0:
         print(f"Tries left: {tries}")
         guess = input("Guess a letter or the whole word:")
+        if guess.lower() =="giveup":
+            print("(╯°□°）╯︵ ┻━┻\nYou've given up! The animal was", word)
+            break
         tries -=1
         if guess.lower() == word.lower():
             print(''' 
@@ -47,8 +45,8 @@ def guess_word():
         else:
             print("Incorrect. Guess again!")
     
-    if not correct_guess:
-        print("Out of tries! The animal was", randomanimal())
+    if not correct_guess and guess.lower() != "giveup":
+        print("Out of tries! The animal was", word)
 
 # Start program
 guess_word()
