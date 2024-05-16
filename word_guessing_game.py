da0f3e3bc0ed146bfe41c10f77f82b679dab03cc
baumpyru <dlee0@terpmail.umd.edu>
2024-05-15 22:08:44 -0400

    Update word_guessing_game.py
    Instead of subtracting from 25, start from 0 and add to get number of tries. Add a status bar so the player knows what the word looks like and their correct guesses. Spell "Contratulations" correctly

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -6,7 +6,7 @@ import random
 name = input("Enter your name:")
 
 # Tell player a quick intro to word guessing game
-print(f"Hello {name}, this game's premise is simple, guess the animal name or a letter before you run out of tries.\nYou start with 25 tries, getting to the highest number wins!")
+print(f"Hello {name}, this game's premise is simple, guess a letter or the entire animal before you hit 25 tries.\nGetting to the lowest number wins!")
 print(''' ██████   ██████   ██████  ██████      ██      ██    ██  ██████ ██   ██ ██ 
 ██       ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      ██  ██  ██ 
 ██   ███ ██    ██ ██    ██ ██   ██     ██      ██    ██ ██      █████   ██ 
@@ -24,15 +24,17 @@ def randomanimal():
 # Game code
 def guess_word():
     word = randomanimal()
-    tries = 25
+    tries = 0
     correct_guess = False
-    while not correct_guess and tries > 0:
-        print(f"Tries left: {tries}")
+    guessed_word = "_" * len(word)
+    print("Word to guess:", ' '.join(guessed_word))
+    while not correct_guess and tries < 25:
+        print(f"Tries left: {25 - tries}")
         guess = input("Guess a letter or the whole word:")
         if guess.lower() =="giveup":
             print("(╯°□°）╯︵ ┻━┻\nYou've given up! The animal was", word)
             break
-        tries -=1
+        tries +=1
         if guess.lower() == word.lower():
             print(''' 
                      ██████  ██████  ██████  ██████  ███████  ██████ ████████ ██ 
@@ -40,8 +42,14 @@ def guess_word():
                     ██      ██    ██ ██████  ██████  █████   ██         ██    ██ 
                     ██      ██    ██ ██   ██ ██   ██ ██      ██         ██       
                      ██████  ██████  ██   ██ ██   ██ ███████  ██████    ██    ██''')
-            print(f"Contratulations, you guessed the word in {tries} tries")
+            print(f"Congratulations, you guessed the word in {tries} tries")
             correct_guess = True
+        elif len(guess) == 1 and guess in word:
+            print("You've correctly guessed a letter!")
+            for i, char in enumerate(word):
+                if char == guess:
+                    guessed_word = guessed_word[:i] + guess + guessed_word[i+1:]
+            print("Word to guess:", ' '.join(guessed_word))
         else:
             print("Incorrect. Guess again!")
     
@@ -49,4 +57,5 @@ def guess_word():
         print("Out of tries! The animal was", word)
 
 # Start program
-guess_word()
+if __name__ == "__main__":
+    guess_word()
